c32a934dd02ff13fd83ba451e50b69bc78f708a6
Fvercellone <th3r34p3r719@gmail.com>
2026-04-20 20:56:56 -0300

    .
    Hice que al traer los datos de la DB los valores DBNULL puedan cargarse en el datagridview como n/a

--- TP1-Comercio/Conexion/manjadorProductos.cs
+++ TP1-Comercio/Conexion/manjadorProductos.cs
@@ -15,20 +15,29 @@ namespace Conexion
             ConexionDB conexion = new ConexionDB();
             try
             {
-                conexion.settearConsulta("SELECT P.Id, P.Nombre, P.Descripcion, M.Descripcion as Marca, C.Descripcion as Categoria, I.ImagenUrl, P.Precio FROM dbo.articulos as P, dbo.imagenes as I, dbo.marcas as M, dbo.categorias as C WHERE P.Id = I.Idarticulo AND M.id = P.IdMarca AND c.Id = P.IdCategoria");
+                conexion.settearConsulta("SELECT P.Id, P.Nombre, P.Descripcion, M.Descripcion as Marca, C.Descripcion as Categoria, I.ImagenUrl, P.Precio FROM dbo.articulos P LEFT JOIN dbo.imagenes I ON P.Id = I.Idarticulo LEFT JOIN dbo.marcas M ON M.Id = P.IdMarca LEFT JOIN dbo.categorias C ON C.Id = P.IdCategoria");
                 conexion.ejecutarLectura();
                 while (conexion._lector.Read())
                 {
                     Producto aux = new Producto();
-                    Marca auxM = new Marca();
-                    Categoria auxC = new Categoria();
 
                     aux.Id = (int)conexion._lector["Id"];
                     aux.Name = (string)conexion._lector["Nombre"];
                     aux.Description = (string)conexion._lector["Descripcion"];
-                    auxM.idMarca = (int)conexion._lector["Marca"];
-                    auxC.idCategoria = (int)conexion._lector["Categoria"];
-                    aux.Imagen = (string)conexion._lector["ImagenUrl"];
+                    aux.Marca = new Marca();
+                    if (conexion._lector["marca"] != DBNull.Value)
+                        aux.Marca.descripcion = (string)conexion._lector["Marca"];
+                    else
+                        aux.Marca.descripcion = "n/a";
+                    aux.Categoria = new Categoria();
+                    if (conexion._lector["categoria"] != DBNull.Value)
+                        aux.Categoria.descripcion = (string)conexion._lector["Categoria"];
+                    else
+                        aux.Categoria.descripcion = "n/a";
+                    if(conexion._lector["ImagenUrl"] != DBNull.Value)
+                        aux.Imagen = (string)conexion._lector["ImagenUrl"];
+                    else
+                        aux.Imagen = "n/a";
                     aux.Precio = (decimal)conexion._lector["Precio"];
                     lista.Add(aux);
                 }
